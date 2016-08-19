4f477a0dffbe5cc3059d98599d3bac78844ce148
suzeyu <suzeyu_1992@163.com>
2016-08-19 17:03:37 +0800

    蓝牙ble扫描功能添加_2

---
+++ app/src/main/java/com/szysky/note/ble/low/DeviceInfoActivity.java
@@ -0,0 +1,23 @@
+package com.szysky.note.ble.low;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.szysky.note.ble.R;
+
+/**
+ * author : suzeyu on 16/8/19.
+ *
+ * blog   : http://szysky.com
+ * github : https://github.com/suzeyu1992
+ */
+
+
+public class DeviceInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_info);
+    }
+}
